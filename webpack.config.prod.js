b7c1da533f5da5adde3ac3b4544395e50553cddc
Vitali.Rusau <Vitali_Rusau@epam.com>
2017-12-19 11:15:55 +0300

    Removed plugins from node_modules

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,13 @@
 const webpack = require('webpack');
 const config = require('./webpack.config.common');
 
-config.plugins.push(new webpack.DefinePlugin({ DEBUG: false }));
+config.plugins.push(new webpack.DefinePlugin({
+    DEBUG: false
+}));
+config.plugins.push(new webpack.optimize.UglifyJsPlugin({
+    compress: {
+        warnings: false
+    }
+}));
 
 module.exports = config;
