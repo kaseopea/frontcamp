506d28e77c5280ffd51e97460a9c54e01527d67a
Vitali.Rusau <Vitali_Rusau@epam.com>
2018-02-20 17:04:16 +0300

    Frontcamp React 1 Random fixes

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '../scss/style.scss';
-import App from './app';
+import App from './appComponent';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
