d418c7ced47653e1df75f28a03bd8e8708c9db01
Vitali.Rusau <Vitali_Rusau@epam.com>
2018-03-20 12:13:09 +0300

    Frontcamp React 1 SSR Fixes Random fixes

--- src/js/components/AddPlip/AddPlip.js
+++ src/js/components/AddPlip/AddPlip.js
@@ -2,44 +2,45 @@ import React, {Component} from 'react';
 import uuid from 'uuid';
 
 class AddPlip extends Component {
-
     constructor(props) {
         super(props);
-
-        this.state = {
-            newPlip: {}
-        };
+        this.state = {};
     }
 
     submitHandler(event) {
-        const author = this.props.author;
-        const plipContent = this.refs.plip.value;
-
+        const {author} = this.props;
         // todo: remove author from props
-
-        if (!plipContent.length || !author.length) {
-            console.error('Not enough data specified');
-        } else {
-            this.setState({
-                newPlip: {
-                    id: uuid.v4(),
-                    date: new Date(Date.now()),
-                    content: plipContent,
-                    author: author
-                }
-            }, () => this.props.onAddPlipSubmit(this.state.newPlip));
+        if (!author.length) {
+            console.error('Not enough data specified to add plip!');
+            return;
         }
+
+        this.setState({
+            id: uuid.v4(),
+            date: new Date(Date.now()),
+            content: this.state.content,
+            author
+        }, () => this.props.onAddPlipSubmit(this.state));
+
         event.preventDefault();
+    }
+
+    onInputChange(event) {
+        this.setState({
+            content: event.target.value
+        });
     }
 
     render() {
         return (
             <div className="add-plip">
-                <form className="pure-form" onSubmit={this.submitHandler.bind(this)}>
+                <form className="pure-form" onSubmit={(e) => this.submitHandler(e)}>
 
                     <div>
-                        <input type="text" ref="plip" placeholder="Plip text"/>
-                        <input type="submit" className="pure-button pure-button-primary add-plip-submit" value="Plip now!"/>
+                        <input type="text" ref="plip" onChange={(e) => this.onInputChange(e)}
+                               placeholder="Plip text"/>
+                        <input type="submit" className="pure-button pure-button-primary add-plip-submit"
+                               value="Plip now!"/>
                     </div>
 
                 </form>
