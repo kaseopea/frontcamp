22185ced47c3144ee6708415df5ad9980ffe3568
Vitali.Rusau <Vitali_Rusau@epam.com>
2018-03-21 17:22:30 +0300

    Frontcamp Angular 1 Component Approach added todos.json

--- server/app.js
+++ server/app.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const appConfig = require('./config.json');
-const path = require('path');
-
+const todosJson = require('./mockData/todos.json');
 const app = express();
 
 /* Setting alternative views directory */
 app.use(express.static('public'));
 
+/* send todos */
+app.get('/todos.json', (req, res) => {
+    res.json(todosJson);
+});
+
 /* uncaughtException error handling */
-// process.on('uncaughtException', (err) => console.log(err));
+process.on('uncaughtException', (err) => console.log(err));
 
 /* Server */
 app.listen(appConfig.appPort, () => console.log(`${appConfig.appName} listening on port ${appConfig.appPort}!`));
