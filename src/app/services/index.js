410521beefe540c4ef670bd7c5c81ab3a845ecf4
Vitali.Rusau <Vitali_Rusau@epam.com>
2018-03-28 15:43:30 +0300

    Frontcamp Angular 2 Random fixes

--- src/app/services/index.js
+++ src/app/services/index.js
@@ -4,12 +4,10 @@ import CONFIG from '../app.config';
 import ArticleStore from './articleStore/articleStore';
 import ArticlesResource from './articleResource/articlesResource';
 import PaginationService from './paginationService/paginationService';
-import uiRouterDebug from './uiRouterDebug/uiRouterDebug';
 
 export const servicesModule = CONFIG.appName.concat('.services');
 
 angular.module(servicesModule, [])
     .service('ArticleStore', ArticleStore)
     .service('PaginationService', PaginationService)
-    .service('UiRouterDebug', uiRouterDebug)
     .factory('ArticlesResource', ArticlesResource);
